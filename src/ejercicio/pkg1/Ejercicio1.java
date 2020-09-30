39d52c75d834bec295a94b814bdb97e015ad36f8
luis <lagonzalez1688@gmail.com>
2020-09-30 01:02:35 -0700

    Ejercicio-1 variable

---
+++ src/ejercicio/pkg1/Ejercicio1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg1;
+
+
+public class Ejercicio1 {
+                
+     public static void main(String[] args) {
+                 int n=5;
+                double a=4.56;
+                char c='a';
+              
+                System.out.println("la suma de las variables es: " + (a+n));
+                System.out.println("la suma de las variables es: " + (a-n));
+                System.out.print("la suma de las variables es: " + (c));
+                
+    }
+    
+}
